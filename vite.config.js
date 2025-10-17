5d60ee47d68fcc762a575281a9246e116305eb9a
Marco C. Pantonial <marcopantonial3@gmail.com>
2025-10-17 09:18:04 +0100

    manifest

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,24 @@ export default defineConfig({
             input: [
                 'resources/css/app.css', 
                 'resources/js/app.js',
+                'resources/js/login/index.js',
+                // Dashboard
+                'resources/js/home.js',
+                // PR
+                'resources/js/PR/index.js',
+                'resources/js/PR/create.js',
+                'resources/js/PR/update.js',
+                'resources/js/PR/process.js',
+                'resources/js/PR/tracking.js',
+                // Users
+                'resources/js/User/Index.js',
+                'resources/js/User/update.js',
+                // Supplemental
+                'resources/js/supplemental/index.js',
+                'resources/css/supplemental.css',
                 // 'resources/js/User/User.js',
+                 // Supplemental
+                 'resources/js/rfq/index.js',
             ],
             refresh: true,
         }),

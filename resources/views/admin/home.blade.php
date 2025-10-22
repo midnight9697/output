8805e845e389acf6be0c0cbcaf7782b090923162
Babyknight08 <marcopantonial3@gmail.com>
2025-10-22 07:59:09 +0000

    environmental key

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -143,6 +143,7 @@ h1 { text-align: center; }
             </div>
         </div>
 </div>
+<script async src="{{env('MARCO_MAP_API')}}"></script>
 @endsection
 @section('custom_js')
     @vite(['resources/js/home.js'])

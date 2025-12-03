a8a00ba7bba80783d13253281edc4eac32682b3d
Marco Pantonial <marcopantonial3@gmail.com>
2025-12-04 07:56:47 +0800

    small updates

---
+++ resources/views/admin/rfq/preview-rules-table.blade.php
@@ -0,0 +1,20 @@
+<table style="width: 100%;margin:10px">
+    <tr>
+        <td class="verticalTop colNumber">1.</td>
+        <td class="verticalTop colSpacing">
+            <h3 class="textWeightNormal">
+                The Department of Environment and Natural Resources - Environmental Management Bureau (DENR- EMB-R8) hereinafter referred to as the "Purchaser" now requests for submission of price quotations for the procurement of the aforesaid item described in the Technical Specifications.'
+            </h3>
+        </td>
+    </tr>
+    <tr>
+        <td class="verticalTop colNumber">2.</td>
+        <td class="verticalTop colSpacing">
+            <h3 class="textWeightNormal">
+                A set of technical specifications are provided in Attachment 1. Bidders must quote only based on
+                their available stock/s except for order basis (e.g., ICT/Laboratory equipment) and shall
+                indicate clearly the specifications, brand or model of the items/menu for catering being offered.
+            </h3>
+        </td>
+    </tr>
+</table>

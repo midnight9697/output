25ef6281256bdbc0dea1fc3a9987839aae3bc792
Marco C. Pantonial <marcopantonial3@gmail.com>
2025-12-09 14:16:43 +0800

    build number

---
+++ public/version/build/assets/action-2a683396.js
@@ -0,0 +1,50 @@
+import{B as o,M as l,S as d,h as m}from"./login-2eaeddfa.js";import{p}from"./pagination-008a36c2.js";import{u as f}from"./User-18220aaa.js";import{G as v}from"./Validation-8d628f68.js";var c=null;class n{constructor(){c=this,this.btnCreateFinalize=document.getElementById("createUserFinalize")}getSection(e){let i="";e.forEach(a=>{i+=`
+          <option value="${a.id}">${a.section}</option>
+        `}),document.getElementById("section").innerHTML=i}createUser(e,i){e.preventDefault();let a={};if(!v.form.form("is valid"))return 0;$("#formCreateUser :input").prop("readonly",!0),o.start(new n().btnCreateFinalize),$("#formCreateUser").serializeArray().forEach(s=>{a[s.name]=s.value}),f.createUser(a,s=>{o.load(document.getElementById("createUserFinalize"),()=>{new n().failedAction("formCreateUser"),i.table.ajax.reload(),l.success("User Successfully Created")},"REGISTER USER")},s=>{o.load(document.getElementById("createUserFinalize"),()=>{$("#formCreateUser .message").html(""),$("#formCreateUser :input").prop("readonly",!1),l.fail(s.message)},"REGISTER USER")})}failedAction(e){$("#"+e).form("reset"),$("#"+e+" .message").html(""),$("#"+e+" :input").prop("readonly",!1)}createUserAction(){var e=new n;d.getSection($("#division").val(),e.getSection),$("#modalCreate").modal("show")}sectionGetAction(e){var i=new n;d.getSection(e.target.value,i.getSection)}fetchUsers(e){e=e.data,console.log(e);let i="";e.forEach(a=>{i+=`
+          <div class="two wide computer eight wide tablet sixteen wide mobile column">
+              <div class="ui card">
+                  <div class="ui slide masked reveal image">
+                      <div class="ui fade reveal">
+                          <div class="visible content">
+                              <img src="files/images/square-image.png" class="visible content">
+                          </div>
+                          <div class="hidden content">
+                              <img src="files/images/middle.avif" class="visible content">
+                          </div>
+                      </div>
+                  </div>
+                  <div class="content">
+                      <a class="header truncate">${a.profile.firstname+" "+a.profile.middlename.charAt(0).toUpperCase()+". "+a.profile.lastname}</a>
+                      <div class="meta">
+                        <span class="description truncate">${a.profile.position}</span>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        `}),usersElement.innerHTML=i}fetchUsersTable(e){const i=document.getElementsByClassName("users_content")[0];var a=[];let s=e;e=e.data;let r="";e.forEach(t=>{a.push({title:t.profile.firstname+" "+(t.profile.middlename=="waived"?"":t.profile.middlename.charAt(0).toUpperCase()+".")+" "+t.profile.lastname}),r+=`
+          <tr>
+            <td>
+              <h4 class="ui image header">
+                <img src="files/images/square-image.png" class="ui mini rounded image">
+                <div class="content">
+                  ${t.profile.firstname+" "+(t.profile.middlename=="waived"?"":t.profile.middlename.charAt(0).toUpperCase()+".")+" "+t.profile.lastname}
+                  <div class="sub header">
+                    ${t.profile.position}
+                  </div>
+                </div>
+              </h4>
+            </td>
+            <td>
+              ${t.profile.division.division}
+            </td>
+            <td>
+              ${t.profile.section.section}
+            </td>
+            <td>
+              ${m.humanDate(t.created_at,"s")}
+            </td>
+            <td>
+              <a href="./users/${t.id}/edit" class="ui button positive tiny user-update-button" data-userid="${t.id}">UPDATE</a>
+            </td>
+          </tr>
+        `}),p.load(document.getElementById("page_content"),s,c.fetchUsersTable,"./api/users/getpage"),i.innerHTML=r}}new n;
---
+++ public/version/build/assets/custom_table-ff526490.js
@@ -0,0 +1,15 @@
+class d{constructor(){this.downAction=!1,this.updateAction=!1,this.viewAction=!1,this.deleteAction=!1,this.udpateName="Update",this.viewName="View",this.downName="Download",this.deleteName="Delete",this.deleteClassName="",this.updateClassName="",this.data={},this.popup=document.createElement("div"),this.menu=document.createElement("div"),this.options_button=document.createElement("div")}loadButtons(l){let t=document.createElement("a"),n=document.createElement("div"),s=document.createElement("div"),e=document.createElement("div"),i=document.createElement("div"),a=document.createElement("div"),o=document.createElement("div");Object.keys(this.data).forEach(c=>{n.dataset[c]=this.data[c],s.dataset[c]=this.data[c],e.dataset[c]=this.data[c]}),o.innerHTML=`
+          <i class="ellipsis vertical icon"></i>
+        `,t.innerHTML=`
+        ${this.downName}
+        `,n.innerHTML=`
+        ${this.udpateName}
+        `,s.innerHTML=`
+          ${this.viewName}
+        `,e.innerHTML=`
+          ${this.deleteName}
+        `,t.className="item link",n.className="item link",s.className="item link",e.className="item link "+this.deleteClassName,n.className="item link "+this.updateClassName,o.className="ui icon button custom-options-button",i.className="ui popup custom-popup",a.className="ui vertical very tiny menu",t.target="__blank",t.href=this.downAction,n.onclick=this.updateAction,s.onclick=this.viewAction,e.onclick=this.deleteAction,l.appendChild(o),this.downAction&&a.appendChild(t),this.updateAction&&a.appendChild(n),this.viewAction&&a.appendChild(s),this.deleteAction&&a.appendChild(e),i.appendChild(a),l.appendChild(i),this.relinitialize()}createCustomButton(l,t,n,s,e={}){const i=document.createElement("div");let a=Object.keys(e);console.log(e),a.forEach(o=>{i.dataset[o]=e[o]}),i.dataset.uid=Date.now().toString(),i.innerHTML=`
+       ${t}
+      `,i.className="item link "+n,this.popup.className="ui popup custom-popup",this.menu.className="ui vertical very tiny menu",this.menu.style="text-align:center",i.onclick=o=>s(e),s&&this.menu.appendChild(i),this.popup.appendChild(this.menu),l.appendChild(this.popup)}loadCustomBtns(l){this.resetBtns();let t=document.createElement("div");t.innerHTML=`
+        <i class="ellipsis vertical icon"></i>
+      `,t.className="ui icon button custom-options-button",l.appendChild(t)}resetBtns(){this.popup=document.createElement("div"),this.menu=document.createElement("div"),this.options_button=document.createElement("div")}relinitialize(){$(".custom-options-button").popup({popup:$(".custom-popup"),on:"click",position:"bottom right",hoverable:!0,closable:!0})}}const p=new d;export{p as T,d as t};
